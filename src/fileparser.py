cae7e2e2c94015f92133862df16474c9d29d1588
Anand Mahamuni <optimisticsnivy7203@proton.me>
2025-09-05 12:11:27 +0530

    Can play audio!!!

--- src/fileparser.py
+++ src/fileparser.py
@@ -1,7 +1,9 @@
 import argparse
+from ffpyplayer.player import MediaPlayer
 from pathlib import Path
 
-file_formats = [ ".mp3", ".flac"]
+file_formats = [".mp3", ".flac"]
+files = []
 parser = argparse.ArgumentParser()
 parser.add_argument("path", help="Add a path containing Music")
 
@@ -10,4 +12,12 @@ target_dir = Path(args.path)
 
 for entry in target_dir.iterdir():
     if entry.suffix in file_formats:
-        print(entry)
+        files.append(str(entry))
+
+print(files)
+player = MediaPlayer(files[0])
+val = " "
+while val != "eof":
+    frame, val = player.get_frame()
+    if val != "eof" and frame is not None:
+        img, t = frame
