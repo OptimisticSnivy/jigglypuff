61a9350b1bc2956a9c8e4169367cdcafad7287a1
Anand Mahamuni <optimisticsnivy7203@proton.me>
2025-09-23 13:11:03 +0530

    Cleanup

--- src/main.py
+++ src/main.py
@@ -7,23 +7,12 @@ files = []
 file_formats = [".mp3", ".flac"]
 
 
-def playQueue(files):
-    for i in range(0, len(files)):
-        player = MediaPlayer(files[i])
-        val = " "
-        while val != "eof":
-            frame, val = player.get_frame()
-            if val != "eof" and frame is not None:
-                img, t = frame
-
-
 # optimize it?
 def walkDir(target_dir):
     for entry in target_dir.iterdir():
         if entry.suffix in file_formats:
-            files.append(str(entry))
+            files.append(entry)
         elif os.path.isdir(entry):
-            print(entry)
             walkDir(entry)
 
     return files
@@ -37,8 +26,18 @@ def getQueue():
     target_dir = Path(args.path)
     files = walkDir(target_dir)
 
-    print(files)
-    # playQueue(files)
+    playQueue(files)
+
+
+def playQueue(files):
+    for i in range(0, len(files)):
+        print("Now Playing:-", files[i].name)
+        player = MediaPlayer(str(files[i]))
+        val = " "
+        while val != "eof":
+            frame, val = player.get_frame()
+            if val != "eof" and frame is not None:
+                img, t = frame
 
 
 getQueue()
